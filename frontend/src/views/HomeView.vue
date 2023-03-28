2054e7a87c617e949989713bc4a5fa244869ab5c
David Tang <david.tang@epfl.ch>
2023-03-28 17:17:29 +0200

    feat: add epfl logo

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 import WebMap from "@/components/WebMap.vue";
+
+const logos: { img: string; url: string }[] = [
+  {
+    img: "EPFL_Logo_184X53.svg",
+    url: "https://www.epfl.ch",
+  },
+];
 </script>
 
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="12">
+      <v-col>
         <h1>{{ $t("map.title") }}</h1>
+        <p class="text-subtitle-1">{{ $t("map.subtitle") }}</p>
+      </v-col>
+      <v-col
+        v-for="logo in logos"
+        :key="logo.img"
+        class="d-flex align-center"
+        cols="auto"
+      >
+        <a :href="logo.url" target="_blank">
+          <v-img :src="logo.img" width="100px" />
+        </a>
       </v-col>
     </v-row>
     <WebMap />
