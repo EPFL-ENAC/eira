c305cdc859e6fe5f406f0f9adac93dae1c182bd8
pierreleripoll <pierreripoll13@gmail.com>
2023-11-14 01:07:11 +0100

    feat(story): add image optimizer #1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from "node:url";
 
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
+
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -15,6 +18,17 @@ export default defineConfig({
         dirname(fileURLToPath(import.meta.url)),
         "./src/locales/**"
       ),
+    }),
+    ViteImageOptimizer({
+      png: {
+        quality: 85,
+      },
+      jpeg: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
     }),
   ],
   resolve: {
